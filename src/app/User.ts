ddbcc74adc740d7ad244b4b964c22434c62c7a39
iamharshkumar <hk7206210@gmail.com>
2020-03-02 11:24:22 +0530

    make User constructor instance public and change registration input to ngModel user.firstName

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,4 +1,9 @@
-export class User{
-    constructor(firstName: String, lastName: String, email: String, userName:String, password:String) {
-    }
+export class User {
+  constructor(
+    public firstName: String,
+    public lastName: String,
+    public email: String,
+    public userName: String,
+    public password: String) {
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../User';
-import { UserRegistrationService } from '../user-registration.service';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../User';
+import {UserRegistrationService} from '../user-registration.service';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +9,11 @@ import { UserRegistrationService } from '../user-registration.service';
 })
 export class RegisterComponent implements OnInit {
 
-  user:User=new User("","","","","");
-  message:any ;
+  user = new User('', '', '', '', '');
+  message: any;
 
-  constructor(private service: UserRegistrationService) { }
+  constructor(private service: UserRegistrationService) {
+  }
 
   ngOnInit() {
   }
